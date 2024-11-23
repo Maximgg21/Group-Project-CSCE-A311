8db1bbae2648234bc664f77b579a04f8b66bdc2c
Wa Xiong <waxiong77@gmail.com>
2024-11-22 17:14:05 -0900

    Updates

--- Player.cpp
+++ Player.cpp
@@ -4,3 +4,14 @@ string Player::getName()
 {
 	return name;
 }
+
+void Player::move(int new_x, int new_y)
+{
+	this->x = new_x;
+	this->y = new_y;
+}
+
+void Player::attack(Player& opponent)
+{
+	opponent.HP--;
+}
--- Player.h
+++ Player.h
@@ -5,10 +5,15 @@ using namespace std;
 class Player
 {
 public:
-	Player(string n = "", int x = 0, int y = 0, int h = 20, int a = 5, int r = 0) : name(n), x(x), y(y), HP(h), attack(a), resources(r) {};
+	Player(string n = "", int x = 0, int y = 0, int h = 20, int r = 0) : name(n), x(x), y(y), HP(h), resources(r) {};
 	string getName();
+	void move(int new_x, int new_y);
+	void attack(Player& opponent);
+
+
+
 private:
-	int x, y, HP, attack, resources;
+	int x, y, HP, resources;
 	string name;
 };
 
